350cbe9b42b408a0a99aa517b94419237564cf6e
Bailey Taber <bailey@Baileys-MacBook-Pro.local>
2021-02-26 21:25:39 -0700

    dunzo'

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, jsonify
 import numpy as np
 import pandas as pd
@@ -5,10 +6,9 @@ import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func, inspect
+from sqlalchemy import create_engine, func, inspect, distinct
 import datetime as dt
 from datetime import datetime, date, time
-from sqlalchemy import distinct
 
 engine=create_engine('sqlite:///Resources/hawaii.sqlite')
 session=Session(engine)
@@ -21,15 +21,20 @@ app=Flask(__name__)
 @app.route('/')
 def home():
 	return(
-	f'<h2>Available Routes:<br/>'
-	f'/api/v1.0/precipitation<br/>'
-	f'/api/v1.0/stations<br/>'
-	f'/api/v1.0/tobs<br/>'
-	f'/api/v1.0/<start><br/>'
-	f'/api/v1.0/<start>/<end>')
+		f'<h2>SQLALCHEMY-CHALLENGE</h2>'
+		f'<h3>Available Routes:<br/></h3>'
+		f'<strong>Date Input Format --> YYYY-MM-DD<br/></strong>'
+		f'<br/>'
+		f"/api/v1.0/precipitation<br/>"
+		f'/api/v1.0/stations<br/>'
+		f'/api/v1.0/tobs<br/>'
+		f'/api/v1.0/start_date<br/>'
+		f'/api/v1.0/start_date/end_date<br/>'
+	)
 
-@app.route('/api/v1.0/precipitation')
+@app.route("/api/v1.0/precipitation")
 def prcp():
+	session=Session(engine)
 	p_query=session.query(measure.date, measure.prcp)\
 		.filter(measure.date>='2016-08-23')\
 		.filter(measure.date<='2017-08-23').all()
@@ -44,6 +49,7 @@ def prcp():
 
 @app.route('/api/v1.0/stations')
 def stations():
+	session=Session(engine)
 	s_query=session.query(station.station).all()
 	all_stations=list(np.ravel(s_query))
 	return jsonify(all_stations)
@@ -51,6 +57,7 @@ def stations():
 
 @app.route('/api/v1.0/tobs')
 def temp():
+	session=Session(engine)
 	t_query=session.query(station.station, measure.date, measure.tobs)\
 	.filter(measure.date >= "2016-08-24")\
    	.filter(measure.date <= "2017-08-23")\
@@ -59,46 +66,55 @@ def temp():
 	return jsonify(temp_data)
 	session.close()
 
-@app.route('/api/v1.0/<start>')
+@app.route("/api/v1.0/<start>")
 def start(start):
-	start_date = datetime.strptime(start,"%Y-%m-%d").date()
-	temp=session.query(func.min(measure.tobs),func.max(measure.tobs), func.avg(measure.tobs))\
-		.filter(measure.date>=start_date).all()
-	results_temp=list(np.ravel(temp))
-	min_temp= results_temp[0]
-	avg_temp= results_temp[1]
-	max_temp= results_temp[2]
+    session=Session(engine)
 
-	temp_data=[]
-	temp_dict= [{"Start Date": start_date},\
-	{"The min temperature for this date was": min_temp},\
-	{"The average temperature for this date was": avg_temp},\
-	{"The max temperature for this date was": max_temp}]
-	temp_data.append(temp_dict)
-	session.close()
+    temp_calc= session.query(func.min(measure.tobs), func.avg(measure.tobs), func.max(measure.tobs)).\
+        filter(measure.date >= start).all()
 
-	return jsonify(temp_data)
-@app.route("/api/v1.0/<start>/<end>")
-def start_end_date(start, end):
-	start_date = datetime.strptime(start,"%Y-%d-%Y").date()
-	end_date = datetime.strptime(end,"%m-%d-%Y").date()
-	temp = session.query (func.min(Measurement.tobs), func.max (Measurement.tobs), func.avg (Measurement.tobs))\
-    	.filter(Measurement.date >= start_date)\
-    	.filter(Measurement.date <= end_date).all()
-	temp_results = list(np.ravel(temp_calc))
+    results_temp= list(np.ravel(temp_calc))
 
-	min_temp = temp_results[0]
-	max_temp = temp_results[1]
-	avg_temp = temp_results[2]
+    min_temp= results_temp[0]
+    avg_temp= results_temp[1]
+    max_temp= results_temp[2]
 
-	temp_data = []
-	temp_dict = [{"Start Date": start_date},{"End Date": end_date},
-    	{"Minimum Temperature for this date range": min_temp},
-    	{"Maximum Temperature for this date range": max_temp},
-    	{"Average Temperature for this date range": avg_temp}]
-	temp_data.append(temp_dict)
-	return jsonify(temp_dict)
+    temp_data=[]
+    temp_dict= [{"Start Date": start},
+    {"The min temperature for this date was": min_temp},
+    {"The avg temperature for this date was": avg_temp},
+    {"The max temperature for this date was": max_temp}]
+    temp_data.append(temp_dict)
+    session.close()
 
+    return jsonify(temp_data)
+
+
+@app.route("/api/v1.0/temp/<start>/<end>")
+def end(start,end):
+    session=Session(engine)
+
+    trip_calcs= session.query(func.min(measure.tobs), func.avg(measure.tobs), func.max(measure.tobs)).\
+        filter(measure.date >= start).filter(measure.date<=end).all()
+
+    trip_result= list(np.ravel(trip_calcs))
+
+    min_temp_trip= trip_result[0]
+    avg_temp_trip= trip_result[1]
+    max_temp_trip= trip_result[2]
+
+    trip_data=[]
+
+    trip_dict= [{"Start Date": start},
+    
+    {"The min temperature for this date was": min_temp_trip},
+    {"The avg temperature for this date was": avg_temp_trip},
+    {"The max temperature for this date was": max_temp_trip},
+    {"End Date": end}]
+    trip_data.append(trip_dict)
+    session.close()
+
+    return jsonify(trip_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
